8e7426d1497018be3faa0066b055e85f9046de0a
Pistipisti94 <pistipisti57@gmail.com>
2023-09-18 17:21:04 +0200

    félig kész

--- JavaScript - korbe-korbe/korbe.js
+++ JavaScript - korbe-korbe/korbe.js
@@ -1,31 +1,46 @@
 var kep = document.querySelector('#ittakep');
 const szelesseg = window.innerWidth;
 const magassag = window.innerHeight;
-var felle = 0;
-var jobbbal = 0;
-
-
-var sheet = window.document.styleSheets[0];
+var fel = 0;
+var le = magassag / 100;
+var bal = szelesseg / 100;
+var jobb = szelesseg / 100;
+var megyjobb = 0;
+var megyle = 0;
+console.log(magassag);
+console.log(szelesseg);
 kep.innerHTML = '<img id="kep1" src="kepek/teki.png" alt="" style="margin-left: 0px;"></img>';
 
-function mozgat() {
-    x = szelesseg;
-    y = magassag;
-
-    if (jobbbal < x -100) {
-        jobbbal += 10;
-        kep.innerHTML = '<img id="kep1" src="kepek/teki.png" alt="" style="margin-left:' + jobbbal + 'px;"></img>';
-
-}
-    console.log(jobbbal);
-    if (jobbbal == 1820 && felle < y -100){
-        kep.innerHTML = '<img id="kep1" src="kepek/teki.png" alt="" style="margin-left: 1820px; margin-top:'+felle+'px;"></img>';
-        felle+=10;
-
-
+function bal() {
+    if (megyjobb > szelesseg - (jobb * 7) && megyle > magassag - (le * 16)) {
+        kep.innerHTML = '<img id="kep1" src="kepek/teki.png" alt="" style="margin-left:' + x + 'px; margin-top:' + y + 'px;"></img>';
+        x -= bal;
     }
-    console.log(felle);
-   
+
 }
 
-setInterval(mozgat, 10);
+function jobbra() {
+    x = szelesseg - (jobb * 6);
+    y = magassag - (le * 15);
+
+    if (megyjobb < x) {
+        kep.innerHTML = '<img id="kep1" src="kepek/teki.png" alt="" style="margin-left:' + megyjobb + 'px;"></img>';
+        megyjobb += jobb;
+    }
+    else if (megyjobb > szelesseg - (jobb * 7) && megyle < y) {
+        kep.innerHTML = '<img id="kep1" src="kepek/teki.png" alt="" style="margin-left:' + x + 'px; margin-top:' + megyle + 'px;"></img>';
+        megyle += le;
+    }
+
+
+}
+
+setInterval(jobbra, 10);
+
+
+
+
+
+
+
+
