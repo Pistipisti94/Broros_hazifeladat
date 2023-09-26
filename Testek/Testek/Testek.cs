62919e4ac46c9613163685a3aebb94f1e0ff4daa
Pistipisti94 <112941646+Pistipisti94@users.noreply.github.com>
2023-09-26 10:15:55 +0200

    Add files via upload
    Kész

---
+++ Testek/Testek/Testek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testek
+{
+    abstract class Testek
+    {
+        public string neve;
+        public double egy, ketto, harom;
+
+        public Testek(string neve,double egy,double ketto,double harom)
+        {
+            this.neve = neve;
+            this.egy = egy;
+            this.ketto = ketto;
+            this.harom = harom;
+        }
+
+        public override string ToString()
+        {
+            return neve;
+        }
+        abstract public double Terfogat();
+        abstract public double Felszin();
+    }
+    
+}
